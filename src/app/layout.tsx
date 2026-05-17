9582d9e8596611b95ab18f2f389ada2da432d4bc
Lucía A. <91093842+luciaaliagat@users.noreply.github.com>
2026-05-17 01:24:10 -0500

    feat: add react query and provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,38 +1,47 @@
-import React from 'react';
-import { Geist, Geist_Mono } from 'next/font/google';
-import './globals.css';
-import { LocaleProvider } from '@/providers';
-import LayoutClient from '@/app/components/LayoutClient';
-import { APP_DESCRIPTION, APP_NAME, DEFAULT_LOCALE } from '@/shared/constants';
+import React from 'react'
+import { Geist, Geist_Mono } from "next/font/google"
+import "./globals.css"
+import { LocaleProvider, QueryProvider } from '@/providers'
+import LayoutClient from '@/app/components/LayoutClient'
+import { APP_DESCRIPTION, APP_NAME, DEFAULT_LOCALE } from '@/shared/constants'
 
 const geistSans = Geist({
-	variable: '--font-geist-sans',
-	subsets: ['latin'],
+    variable: '--font-geist-sans',
+    subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-	variable: '--font-geist-mono',
-	subsets: ['latin'],
+    variable: '--font-geist-mono',
+    subsets: ['latin'],
 });
 
 export const metadata = {
-	title: {
-		default: APP_NAME,
-		template: `%s - ${APP_NAME}`,
-	},
-	description: APP_DESCRIPTION,
+    title: {
+        default: APP_NAME,
+        template: `%s - ${APP_NAME}`,
+    },
+    description: APP_DESCRIPTION,
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-	return (
-		<html
-			lang={DEFAULT_LOCALE}
-			className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
-			<body className="h-full bg-zinc-50 text-zinc-900">
-				<LocaleProvider>
-					<LayoutClient>{children}</LayoutClient>
-				</LocaleProvider>
-			</body>
-		</html>
-	);
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <html
+            lang={DEFAULT_LOCALE}
+            className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+        >
+            <body className="h-full bg-zinc-50 text-zinc-900">
+                    <QueryProvider>
+                        <LocaleProvider>
+                            <LayoutClient>
+                                {children}
+                            </LayoutClient>
+                        </LocaleProvider>
+                    </QueryProvider>
+            </body>
+        </html>
+    )
 }
